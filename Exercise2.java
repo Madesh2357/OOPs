f5c0bb0f423e67f336fed29c9e411eabfad7bd47
Madesh2357 <iammadeshofficial7@gmail.com>
2025-08-06 02:08:25 -0700

    Create Exercise2.java

---
+++ Exercise2.java
@@ -0,0 +1,29 @@
+package Java;
+
+import java.util.Scanner;
+public class prime_check {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter A positive integer:");
+		int num = sc.nextInt();
+		if(num ==0 || num ==1) {
+			System.out.println(num+" is neither a Prime nor a Composite number");
+		}
+		
+		else {
+			for(int i = 2; i <= num; i++) {
+				if(i % num != 0) {
+					System.out.println(num + "is not a Prime Number");
+					break;
+				}
+				else {
+					System.out.println(num + "is a Prime Number");
+		
+				}
+	}
+}
+}
+}
